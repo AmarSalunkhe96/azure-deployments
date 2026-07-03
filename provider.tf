5fc99d476145b5255077c6b633b56b467d7be6ee
AmarSalunkhe96 <107094245+AmarSalunkhe96@users.noreply.github.com>
2026-07-03 15:45:30 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  # Configured exactly for your remote backend storage account and container
+  backend "azurerm" {
+    resource_group_name  = "my-rg"
+    storage_account_name = "terrstorage96"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
